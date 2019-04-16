17fd903d07dee363e7a3cbeb63e0ff33780e5239
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2019-04-16 21:37:51 +0200

    Required changes made

--- src/bussineslogic/Facade.java
+++ src/bussineslogic/Facade.java
@@ -17,54 +17,63 @@ public class Facade {
 
     public static void main(String[] args) {
         Facade facade = new Facade();
-        String[] productTable = new String[]{"Koszulka biała","139.99","T_SHIRTS","MAN","M","Levis"};
+        String[] productTable = new String[]{"Koszulka biała", "139.99", "T_SHIRTS", "MAN", "M", "Levis"};
+        String[] productTable2 = new String[]{"Spodnie czarne", "259.47", "JEANS", "MAN", "M", "HM"};
         String[] clientTable = new String[]{"1", "Jan", "Kowalski", "Wroclaw1", "732456987", "jan@wp.pl"};
 
         facade.addProduct(productTable);
-        
+        facade.addProduct(productTable2);
 
         String client = facade.addClient(clientTable);
         System.out.println(client);
         client = facade.addClient(clientTable);
         System.out.println(client);
         System.out.println(Arrays.toString(facade.clientList.toArray()));
+
+        System.out.println(facade.addProductToBasket(clientTable, productTable2));
+        System.out.println(facade.removeFromBasket(clientTable, productTable2));
+        System.out.println(facade.addProductToBasket(clientTable, productTable));
+        
+        System.out.println(facade.clientList.get(0));
+
         
         String[] filters=new String[]{"120", "150", "T_SHIRTS", "MAN", "M", "Levis"};
         String basket=facade.browseBasket(clientTable, filters);
         System.out.println(basket);
+        
     }
-    
+
     public ArrayList<String> publicProductsModel() {
         ArrayList<String> products = new ArrayList<>();
         Iterator<Product> help = productList.iterator();
-        while(help.hasNext()){
+        while (help.hasNext()) {
             Product next = help.next();
             products.add(next.toString());
         }
         return products;
     }
-    
+
     public void removeProduct(Product product) {
         Product productExist;
-        
-        if((productExist = searchProduct(product)) != null){
+
+        if ((productExist = searchProduct(product)) != null) {
             productList.remove(productExist);
         }
     }
-    
+
     public void modifyProductPrice(double price, Product product) {
         Product productExist;
-        
-        if((productExist = searchProduct(product)) != null) {
+
+        if ((productExist = searchProduct(product)) != null) {
             productExist.setPrice(price);
         }
     }
 
-    public Product addProduct(String[] productTable){
+    public Product addProduct(String[] productTable) {
         Product product1, productExist;
         Factory factory = new Factory();
         product1 = factory.createProduct(productTable);
-        
+
         if ((productExist = searchProduct(product1)) == null) {
             productList.add(product1);
             return product1;
@@ -73,13 +82,13 @@ public class Facade {
         return null;
     }
 
-    public String addClient(String[] clientTable){
+    public String addClient(String[] clientTable) {
         Factory factory = new Factory();
         Client client = factory.createClient(clientTable);
 
-        if (searchClient(client) == null) { 
-            clientList.add(client); 
-        return client.toString(); 
+        if (searchClient(client) == null) {
+            clientList.add(client);
+            return client.toString();
         }
         return null;
     }
@@ -87,11 +96,11 @@ public class Facade {
     public String addProductToBasket(String[] clientTable, String[] productTable) {
         Factory factory = new Factory();
         Client client = factory.createClient(clientTable);
-        if (searchClient(client) != null) {
+        if ((client = searchClient(client)) != null) {
             Product product = factory.createProduct(productTable);
-            if (searchProduct(product) != null) {
+            if ((product = (searchProduct(product))) != null) {
                 client.addToShoppingBasket(product);
-                return "Dodano" + product.getName();
+                return "Dodano: " + product.getName();
             }
             return "Nie istnieje taki produkt";
         }
@@ -101,9 +110,9 @@ public class Facade {
     public String removeFromBasket(String[] clientTable, String[] productTable) {
         Factory factory = new Factory();
         Client client = factory.createClient(clientTable);
-        if (searchClient(client) != null) {
+        if ((client = searchClient(client)) != null) {
             Product product = factory.createProduct(productTable);
-            if (searchProduct(product) != null) {
+            if ((product = (searchProduct(product))) != null) {
                 return client.removeFromShoppingBasket(product);
             }
             return "Nie istnieje taki klient";
@@ -130,23 +139,17 @@ public class Facade {
         }
         return null;
     }
-    
-    public String browseBasket(String[] clientTable, String [] filters){
+
+    public String browseBasket(String[] clientTable, String[] filters) {
         Factory factory = new Factory();
         Client client, clientExist;
-        client= factory.createClient(clientTable);
+        client = factory.createClient(clientTable);
 
-        if ((clientExist = searchClient(client)) != null) { 
-        return clientExist.browseShoppingBasket(filters); 
+        if ((clientExist = searchClient(client)) != null) {
+            return clientExist.browseShoppingBasket(filters);
         }
         return null;
-       
-        
+
     }
-    
- 
-    
-  
-    
 
 }
--- src/bussineslogic/model/ShoppingBasket.java
+++ src/bussineslogic/model/ShoppingBasket.java
@@ -9,7 +9,6 @@ public class ShoppingBasket {
     private Map<Product, Integer> productMap = new HashMap<>();
     private double totalPrice = 0;
 
-
     public Map<Product, Integer> getProductMap() {
         return productMap;
     }
@@ -19,24 +18,28 @@ public class ShoppingBasket {
     }
 
     public double getTotalPrice() {
-        for (Product p : productMap.keySet()){
+        for (Product p : productMap.keySet()) {
             totalPrice += p.getPrice() * productMap.get(p);
         }
         return totalPrice;
     }
-    
-    public void addProductToShoppingBasket(Product product){
-        for (Product p : productMap.keySet()) {
-            if (p.equals(product)) {
-                productMap.put(p, productMap.get(p) + 1);
-            } else {
-                productMap.put(product, 1);
+
+    public void addProductToShoppingBasket(Product product) {
+        if (!productMap.keySet().isEmpty()) {
+            for (Product p : productMap.keySet()) {
+                if (p.equals(product)) {
+                    productMap.put(p, productMap.get(p) + 1);
+                } else {
+                    productMap.put(product, 1);
+                }
             }
+        }else{
+            productMap.put(product, 1);
         }
     }
 
-    public String removeProductFromShoppingBasket(Product product){
-        String message = "";
+    public String removeProductFromShoppingBasket(Product product) {
+        String message = "Brak takiego produktu w koszyku";
         for (Product p : productMap.keySet()) {
             if (p.equals(product)) {
                 if (productMap.get(p) > 1) {
@@ -50,25 +53,25 @@ public class ShoppingBasket {
         }
         return message;
     }
-    
+
     @Override
     public String toString() {
-        return "ShoppingBasket{" +
-                "Product Map=" + productMap +
-                ", Total Price=" + totalPrice +
-                '}';
+        return "ShoppingBasket{"
+                + "Product Map=" + productMap
+                + ", Total Price=" + totalPrice
+                + '}';
     }
-    
-    public String browseBasket(String[] filters){
-        
-        Map<Product, Integer>  filteredBasket=productMap;
-        for(Product p : productMap.keySet()){
-            if(p.isValid(filters) != null){
+
+    public String browseBasket(String[] filters) {
+
+        Map<Product, Integer> filteredBasket = productMap;
+        for (Product p : productMap.keySet()) {
+            if (p.isValid(filters) != null) {
                 filteredBasket.put(p, productMap.get(p));
             }
         }
-        
-        return "ShoppingBasket{" +
-                "productMap=" + filteredBasket +"}";
+
+        return "ShoppingBasket{"
+                + "productMap=" + filteredBasket + "}";
     }
 }
